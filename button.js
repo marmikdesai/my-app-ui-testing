7d162c9dfc8ccecf43b52ecfdcab36b0cd13698d
marmik <marmikdesai@live.com>
2019-01-10 19:36:28 +0530

    button component
    button component

---
+++ button.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const buttonStyle = {
+  fontSize: '15px',
+  textAlign: 'center'
+}
+
+class Button extends React.Component {
+  render() {
+    return (
+      <button style={divStyle}>Name</button>
+    );
+  }
+}
